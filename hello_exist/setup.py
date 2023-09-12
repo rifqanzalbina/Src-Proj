a8571165fd296a17a67617479442c4ead7641e49
M Rifqan Zalbina <124742008+rifqanzalbina@users.noreply.github.com>
2023-09-12 17:56:15 +0700

    Update setup.py

--- hello_exist/setup.py
+++ hello_exist/setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[],
-    url='https://github.com/rifqanzalbina/',
+    url='https://github.com/rifqanzalbina/Src-Proj/edit/main/hello_exist/setup.py',
     author='Your Name',
     author_email='your.email@example.com',
     description='A simple Python library example'
