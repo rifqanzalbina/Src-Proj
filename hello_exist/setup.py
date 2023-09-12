e3af648cd9bb2ad93e4a4533a7988c6087b10e35
M Rifqan Zalbina <124742008+rifqanzalbina@users.noreply.github.com>
2023-09-12 17:48:37 +0700

    Add files via upload

---
+++ hello_exist/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helloexist',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/rifqanzalbina/theone',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A simple Python library example'
+)
